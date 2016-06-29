f1394084f11907ae6790e5799820d8950a45b563
Aaron Jackson <aaron.jcksn@outlook.com>
2016-06-28 20:25:26 -0500

    Added images and css. Also added jQuery into detect landscape or Portrait.

--- assets/includes/esales_aid.js
+++ assets/includes/esales_aid.js
@@ -1,2 +1,19 @@
 /*  JavaScript Document                      */
 
+
+$(document).ready(function() {
+    setOrientationListener();
+});
+
+function setOrientationListener(){
+	rotationInterval = setInterval( function(){ updateOrientation(); }, 500 );
+}
+
+function updateOrientation(){
+	if($('body').width() < 1024){
+		$('.page').removeClass('landscape').addClass('portrait');
+	}else{
+		$('.page').removeClass('portrait').addClass('landscape');	
+	}
+}
+
